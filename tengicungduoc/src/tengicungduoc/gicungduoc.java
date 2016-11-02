89be151ace0778fa2648637443cf6d0063dcb941
Trung Ca <Windows 8.1 Gamer@WIN-0BIFCMNNGIA>
2016-11-02 15:34:32 +0700

    Init Prọject

---
+++ tengicungduoc/src/tengicungduoc/gicungduoc.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package tengicungduoc;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Windows 8.1 Gamer
+ *
+ */
+public class gicungduoc extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gicungduoc f= new gicungduoc();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+	}
+
+}
